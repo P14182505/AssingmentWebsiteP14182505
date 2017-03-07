a0b0c16e21623cbe3252f7a3f3dff20854b2dc3e
P14182505 <Saulpountney@yahoo.co.uk>
2017-03-07 16:20:37 +0000

    Commit 2 [07/04/17] [3:59:30]
    - edited the style.css -
    +Parallax header residing within a flex box. Image used as background
    (background3.jpg).
    +added section id’s, this includes a differing amounts of padding to
    differentiate from each other.
    
    - edited image folder -
    +added background3.jpg to the folder.
    
    - edited the index.html -
    + added relative imports that allows my website to stretch to all sides.
    +linked my html to my css and js.
    +added a basic navigation links will the ability to scroll when clicked
    in a smooth transition. (index.js)
    +added specific script imports including jQuery and the link to
    index.js.
    
    - edited index.js -
    + added a smooth Scroll javascript function.

--- Portfolio Website/js/index.js
+++ Portfolio Website/js/index.js
@@ -1,10 +1,23 @@
-/*---------------------------------------------------------*/
-/* This will be my javascrip file where all my java resides
------------------------------------------------------------ */
+/*----------------------------------------------------*/
+/* Smooth Scrolling
+------------------------------------------------------ */
 
+jQuery(document).ready(function($) {
 
+   $('.smoothscroll').on('click',function (e) {
+	    e.preventDefault();
 
+	    var target = this.hash,
+	    $target = $(target);
 
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 800, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+  
+});
 
 
 
